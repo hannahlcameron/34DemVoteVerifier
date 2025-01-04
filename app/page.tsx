2cb9b034f0fc93637c4cc5abfb705af656b7e864
hannahlcameron <hannahlisa.cameron@gmail.com>
2025-01-04 15:15:24 -0800

    Added table for voter upload

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { MaterialReactTable } from "material-react-table";
 
 Amplify.configure(outputs);
 
@@ -14,6 +15,7 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [tableData, setTableData] = useState<{ headers: string[], data: Record<string, string>[] } | null>(null);
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -35,26 +37,62 @@ export default function App() {
     });
   }
 
+    function parseTSV(tsv: string) {
+        const lines = tsv.trim().split("\n");
+        const headers = ["vanId", "name", "preferredEmail"];
+        const data = lines.slice(1).map(line => {
+            const values = line.split("\t");
+            return headers.reduce((obj, header, index) => {
+                obj[header] = values[index];
+                return obj;
+            }, {} as Record<string, string>);
+        });
+        return { headers, data };
+    }
+
+    function handleFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const tsv = e.target?.result as string;
+                const parsedData = parseTSV(tsv);
+                setTableData(parsedData);
+            };
+            reader.readAsText(file);
+        }
+    }
+
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li
-              onClick={() => deleteTodo(todo.id)}
-              key={todo.id}>
-              {todo.content}
-          </li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
+      <h1>Upload Voters</h1>
+        <input type="file" accept=".txt" onChange={handleFileUpload} />
+        {tableData && (
+            <MaterialReactTable
+                columns={tableData.headers.map(header => ({ header, accessorKey: header }))}
+                data={tableData.data}
+            />
+        )}
+
+     <h1></h1>
+
+      {/*<button onClick={createTodo}>+ new</button>*/}
+      {/*<ul>*/}
+      {/*  {todos.map((todo) => (*/}
+      {/*    <li*/}
+      {/*        onClick={() => deleteTodo(todo.id)}*/}
+      {/*        key={todo.id}>*/}
+      {/*        {todo.content}*/}
+      {/*    </li>*/}
+      {/*  ))}*/}
+      {/*</ul>*/}
+      {/*<div>*/}
+      {/*  🥳 App successfully hosted. Try creating a new todo.*/}
+      {/*  <br />*/}
+      {/*  <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">*/}
+      {/*    Review next steps of this tutorial.*/}
+      {/*  </a>*/}
+      {/*</div>*/}
     </main>
   );
 }
